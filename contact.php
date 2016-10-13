1400ffee47ce6a318c5d150186ccf75a9bede8fc
chenoajai <winsteadchenoa@gmail.com>
2016-10-13 09:41:59 -0600

    added navbar link

--- contact.php
+++ contact.php
@@ -3,8 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>contact</title>
+
+      <?php require_once("lib/headutils.php");?>
+
   </head>
   <body>
+    <?php require_once("lib/partials/navBar.php");?>
     This is a contact page
   </body>
 </html>
