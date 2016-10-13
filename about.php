805aba0d5bef68d12fd4dfa3a6cbb00a7f6afc7b
rshwnj05 <rshwnj05@yahoo.com>
2016-10-13 09:38:32 -0600

    add headutils stuff

---
+++ about.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php require_once("lib/headutils.php");?>
+
+  </body>
+</html>This is the about page
---
+++ headutils.php
@@ -0,0 +1,21 @@
+<link href='https://fonts.googleapis.com/css?family=Montserrat|Raleway:400,300' rel='stylesheet' type='text/css'>
+
+<!-- Custom CSS -->
+
+<link rel="stylesheet" href="lib/css/bootstrap.css">
+<link rel="stylesheet" href="lib/css/about.css">
+<link rel="stylesheet" href="lib/css/index.css">
+<link rel="stylesheet" href="lib/css/contact.css">
+
+
+
+<!-- Font Awesome -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+  <!-- Bootstrap - Latest compiled and minified JavaScript -->
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"
+      integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ=="
+      crossorigin="anonymous"></script>
