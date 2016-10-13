62f82567f845989002645a5d1e0c06abc650ff06
rasmith78 <roselynsmith78@gmail.com>
2016-10-13 09:54:36 -0600

    added navbar links

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
  <head>
    <meta charset="utf-8">
    <title></title>
-   <link rel="stylesheet" href="lib/css/index.css">
-    <?php require_once("lib/index.php");?>
+
+    <?php require_once("headutils.php");?>
+    <?php require_once("navbar.php");?>
+    <link rel="stylesheet" href="lib/css/index.css">
  </head>
  <body>
-
+   <h1>Welcome teamBlue</h1>
  </body>
 </html>
--- navbar.php
+++ navbar.php
@@ -8,15 +8,15 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">Classes</a>
+        <a class="navbar-brand" href="index.php">Home</a>
       </div>
 
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
-          <li class="active"><a href="#farmington">Farmington <span class="sr-only">(current)</span></a></li>
-          <li><a href="#gallup">Gallup</a></li>
-          <li><a href="#iconArea">Icons</a></li>
+          <li class="active"><a href="about.php">About <span class="sr-only">(current)</span></a></li>
+          <li><a href="contact.php">Contact</a></li>
+
         </ul>
           </li>
         </ul>
